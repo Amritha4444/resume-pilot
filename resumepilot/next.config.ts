22ce512673409917aca603b96700f5661b173188
AmrithaS <amritha.santhoshtv@gmail.com>
2026-07-27 16:02:10 +0530

    fix next config exports

--- resumepilot/next.config.ts
+++ resumepilot/next.config.ts
@@ -1,14 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
   typescript: {
-    // Also ignore typescript errors during build if any pop up
     ignoreBuildErrors: true,
   },
 };
 
-module.exports = nextConfig;
+export default nextConfig;;
